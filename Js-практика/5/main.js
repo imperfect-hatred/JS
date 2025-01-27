e7e6761edba943bf63a3d64c54823459cc86b591
imperfect-hatred <nnikakoj3@gmail.com>
2025-01-27 14:47:44 +0900

    задание 5, 1-2

---
+++ Js-практика/5/main.js
@@ -0,0 +1,52 @@
+// //задание 1
+// const existedUserLogin = "the_best_user";
+// const existedUserPassword = "12345678";
+// const userLogin = prompt("Введите логин".trim());
+// const userPassword = prompt("Введите пароль".trim());
+// if (userLogin === existedUserLogin && userPassword === existedUserPassword) {
+//   alert("привет босс");
+// } else {
+//   alert("Пошол отсюда");
+// }
+// //
+
+// // задание 2
+// const test1 = Number(prompt("Сколько будет 2+2?"));
+// if (test1 === 4) {
+//   alert("Прально!");
+// } else {
+//   alert("УБЬЮ");
+// }
+// const test2 = Number(prompt("Сколько будет 2*2"));
+// if (test2 === 4) {
+//   alert("Прально!");
+// } else {
+//   alert("УБЬЮ");
+// }
+// const test3 = Number(
+//   prompt(
+//     "У Пети было 5 яблок. 3 из них он съел, 1 отдал другу. Сколько яблок у Пети осталось?"
+//   )
+// );
+// if (test3 === 1) {
+//   alert("Прально!");
+// } else {
+//   alert("УБЬЮ");
+// }
+// const test4 = Number(
+//   prompt(
+//     "У Маши было 10 конфет. 2 она съела, 1 отдала другу. После мама дала Маше еще 5 конфет. Сколько в итоге конфет осталось у Маши?"
+//   )
+// );
+// if (test4 === 12) {
+//   alert("Прально!");
+// } else {
+//   alert("УБЬЮ");
+// }
+// const test5 = Number(prompt("Сколько будет 2+2*2?"));
+// if (test5 === 6) {
+//   alert("Прально!");
+// } else {
+//   alert("УБЬЮ");
+// }
+// //
